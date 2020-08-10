da4812c1177b0413a70b60e0596ce699b2baa12f
omarion3698 <omaribinbakarivic@gmail.com>
2020-08-10 18:38:43 +0300

    Add a landing page

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,43 @@
-$(window).load(function(){
-  if ($('#back-to-top').length) {
-    var scrollTrigger = 100,
-    backToTop = function() {
-      var scrollTop = $(window).scrollTop();
-      if (scrollTop > scrollTrigger) {
-        $('#back-to-top').addClass('show');
-      } else {
-        $ ('#back-to-top').removeClass('show');
-      }
-    };
-    backToTop();
-    $(window).on('scroll', function() {
-      backToTop();
-    });
-    $('#back-to-top').on('click', function(e) {
-      e.preventDefault();
-      $('html,body').animate({
-        scrollTop: 0 
-      }, 700);
-      });
+let sliderImages = document.querySelectorAll('.slide'),
+    arrowLeft = document.querySelector('#arrow-left'),
+    arrowRight = document.querySelector('#arrow-right'),
+    current = 0;
+
+function reset() {
+  for(let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = 'none';
   }
+}
+
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = 'block';
+}
+
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = 'block';
+  current--;
+}
+
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = 'block';
+  current++;
+}
+
+arrowLeft.addEventListener('click', function() {
+  if(current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
 });
 
-$(function() {
-  SyntaxHighlighter.all();
-});
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide",
-    start: function(slider) {
-    $('body').removeClass('loading');
-    }
-  });
+arrowRight.addEventListener('click', function() {
+  if(current === sliderImages.length - 1) {
+    current = -1
+  }
+  slideRight();
 });
 
-$(document).on('click', 'a[href^="#"]', function (event) {
-  event.preventDefault();
-
-$('html,body').animate({
-  scrollTop: $($.attr(this, 'href')).offset().top
-}, 500);
-});
+startSlide();

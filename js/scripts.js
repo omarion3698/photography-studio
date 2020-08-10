e9710765ec4c4856bf841e107b041201f4278ff4
omarion3698 <omaribinbakarivic@gmail.com>
2020-08-10 22:11:21 +0300

    Add a scroll function

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,13 @@ $(document).ready(function() {
     event.preventDefault(event);
   });
 });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if(window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})

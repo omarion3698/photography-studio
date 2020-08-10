3696b83b094cba04eb9f7db2013dc1165533ce73
omarion3698 <omaribinbakarivic@gmail.com>
2020-08-10 20:54:34 +0300

    Last update

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,18 @@ arrowRight.addEventListener('click', function() {
 });
 
 startSlide();
+
+$(document).ready(function() {
+  $("inform#contact").submit(function(event) {
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+      alert (name + ", we have received your message. Thank you for contacting us.");
+    }
+    else {
+      alert ("Please enter your name and email!");
+    }
+    event.preventDefault(event);
+  });
+});
